7ba9160d1dd6da4552c2d4b1d9c709be166ab145
Naveed <naveedkhalid091@yahoo.com>
2024-08-04 10:30:54 +0500

    First deplyment done

---
+++ Assignements/projects-deployment/001_firstproject/src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+       <div>
+       <h1>Name: Naveed Khalid</h1>
+        <br/>
+        <p>Hello everyone,</p>
+        <p>Welcome to my first deployed page on Vercel!</p> 
+        <p>I am excited to share this milestone with you all. InshaAllah, I will be deploying several more applications on Vercel very soon. Stay tuned for more updates!</p>
+        <br/>
+        <p>My front-end development skills are almost complete, and I amm looking forward to showcasing more of my work.</p>
+        <p>Hurry up and check out the latest updates!</p>
+      </div>      
+    </main>
+  );
+}
