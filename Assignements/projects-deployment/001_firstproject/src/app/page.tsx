d7d363da235edcb3f20587a07c4f7f6d785d05a9
Naveed <naveedkhalid091@yahoo.com>
2024-08-04 12:04:33 +0500

    first deployment at vercel is done

--- Assignements/projects-deployment/001_firstproject/src/app/page.tsx
+++ Assignements/projects-deployment/001_firstproject/src/app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
         <p>Welcome to my first deployed page on Vercel!</p> 
         <p>I am excited to share this milestone with you all. InshaAllah, I will be deploying several more applications on Vercel very soon. Stay tuned for more updates!</p>
         <br/>
-        <p>My front-end development skills are almost complete, and I amm looking forward to showcasing more of my work.</p>
         <p>Hurry up and check out the latest updates!</p>
       </div>      
     </main>
